cd1184aaad0fca7a76cb80db357339b21ee5a733
Dominique Martins <dominique@lumen.games>
2024-01-12 13:57:00 -0300

    Interaction

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public void Interact();
+}
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public List<IInteractable> interactablesInArea;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out IInteractable interactable))
+        {
+            interactablesInArea.Add(interactable);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out IInteractable interactable))
+        {
+            interactablesInArea.Remove(interactable);
+        }
+    }
+}
